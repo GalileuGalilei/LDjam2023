aa8753d7d28041dc0cbd589d43758ae5d888b28e
GalileuGalilei <alfredocossetin@outlook.com>
2023-05-01 08:29:33 -0300

    oscar para o meu mano righ

---
+++ UnityProject/Assets/Scripts/CutSceneController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class CutSceneController : MonoBehaviour
+{
+    VideoPlayer player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<VideoPlayer>();
+        player.loopPointReached += LoadFase1;
+    }
+
+    private void LoadFase1(VideoPlayer source)
+    {
+        source.Stop();
+        SceneManager.LoadScene("Fase1");
+    }
+}
