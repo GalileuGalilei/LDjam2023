358342731055a2716a886293236c154cf8a14dfe
GalileuGalilei <alfredocossetin@outlook.com>
2023-05-01 05:04:58 -0300

    atrito adicionado

--- UnityProject/Assets/Scripts/CarController.cs
+++ UnityProject/Assets/Scripts/CarController.cs
@@ -5,11 +5,26 @@ using UnityEngine.UIElements;
 
 public class CarController : MonoBehaviour
 {
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     private void OnMouseOver()
     {
         if(Input.GetMouseButtonDown(0)) 
         {
             GameManager.selectedCar = this.gameObject.transform.parent.gameObject;
         }    
+    }
+
+    private void FixedUpdate()
+    {
+        if(Mathf.Abs(rb.velocity.magnitude) > 0)
+        {
+            rb.velocity *= 0.995f;
+        }
     }
 }
